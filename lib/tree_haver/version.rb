d826acd025ab949476651d9f06bffacbfab359fa
Peter H. Boling <peter.boling@gmail.com>
2026-03-27 00:10:47 -0600

    🎨 Template bootstrap by kettle-jem v1.0.0

--- lib/tree_haver/version.rb
+++ lib/tree_haver/version.rb
@@ -1,20 +1,8 @@
 # frozen_string_literal: true
 
 module TreeHaver
-  # Version information for TreeHaver
-  #
-  # This module contains version constants following Semantic Versioning 2.0.0.
-  #
-  # @see https://semver.org/ Semantic Versioning
   module Version
-    # Current version of the tree_haver gem
-    #
-    # @return [String] the version string
     VERSION = "5.0.5"
   end
-
-  # Traditional location for VERSION constant
-  #
-  # @return [String] the version string
-  VERSION = Version::VERSION
+  VERSION = Version::VERSION # Traditional Constant Location
 end
